91ab8b80b200b0a7e9d7992291ded846616b945b
ahernandez <ashby.hernandez@constantcontact.com>
2024-09-17 16:06:45 -0400

    Responsive design for project detail page and image slider

--- src/components/project_detail/image_gallery/myGallery.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ImageGallery from "react-image-gallery";
-
-const MyGallery = ({ images }) => { 
-  return <ImageGallery items={images} />;
-}
-
-export default MyGallery;
